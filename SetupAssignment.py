b4d5b444a5952772bb72d70130426d49383ac962
BeTKH <BeTKH>
2024-10-05 02:15:29 +0000

    Percent Change output

--- SetupAssignment.py
+++ SetupAssignment.py
@@ -10,17 +10,21 @@
 
 # You'll need to replace these values with the specifics for your setup.
 
-storage_end_point = "assign1storage.dfs.core.windows.net" 
+
+storage_end_point = "assign1storebekalue.dfs.core.windows.net" 
 my_scope = "MarchMadnessScope"
-my_key = "assign1-key"
+my_key = "march-madstore-key"
 
 spark.conf.set(
     "fs.azure.account.key." + storage_end_point,
     dbutils.secrets.get(scope=my_scope, key=my_key))
 
 # Replace the container name (assign-1-blob) and storage account name (assign1storage) in the uri.
-uri = "abfss://assign-1-blob@assign1storage.dfs.core.windows.net/"
 
+# my continer name = assign1 , this continer was created for assignment-1 and contins the SandP500Daily.csv file 
+# my data lake storage end point is: [assign1storebekalue.dfs.core.windows.net/]
+
+uri = "abfss://assign1@assign1storebekalue.dfs.core.windows.net/"
 
 # COMMAND ----------
 
@@ -31,7 +35,7 @@ display(sp_df)
 
 # COMMAND ----------
 
-# Create new column with the range for the day.
+# Create new column with the range for the day. ( each row represents the day)
 sp_range_df = sp_df.withColumn('Range', sp_df.High - sp_df.Low)
 
 display(sp_range_df)
@@ -39,6 +43,8 @@ display(sp_range_df)
 # COMMAND ----------
 
 # Save this range file to a single CSV.  Use coalesce to output it to a single file.
+
+# the output is written to the object storage / continer  "assign1" and the path is : "/output/Range" 
 sp_range_df.coalesce(1).write.option('header',True).mode('overwrite').csv(uri+"output/Range")
 
 
@@ -56,8 +62,26 @@ sp_range_df.coalesce(1).write.option('header',True).mode('overwrite').csv(uri+"o
 
 
 
+from pyspark.sql import functions as F
+# percent change = ((High - Low) / Open) * 100
+
+
+sp_range_df = sp_range_df.withColumn( "Percent_Change", 
+                                     ((F.col("High") - F.col("Low")) / F.col("Open")) * 100)
+
+# Sort Percent_Change column in descending order
+sorted_sp_range_df = sp_range_df.orderBy(F.col("Percent_Change").desc())
+
+# result
+
+display(sorted_sp_range_df)
+
+
+
 # COMMAND ----------
 
 # Save the file to a single CSV file to your storage account to a single CSV file in the location output/PercentChange.
 
+# Using Coalesce to save to a single CSV 
+sorted_sp_range_df.coalesce(1).write.option('header', True).mode('overwrite').csv(uri + "output/PercentChange")
 
